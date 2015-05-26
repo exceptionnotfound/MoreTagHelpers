8046e64b3b6704f00fe7d1e50b22afdd612e0b5c
exceptionnotfound <mpjones428@gmail.com>
2015-05-25 22:56:45 -0700

    Removed excess comments, finalized code for post.

--- src/MoreTagHelpers/ViewModels/AddMovieVM.cs
+++ src/MoreTagHelpers/ViewModels/AddMovieVM.cs
@@ -8,26 +8,27 @@ using System.Threading.Tasks;
 
 namespace MoreTagHelpers.ViewModels
 {
-    public class AddMovieVM
-    {
-        [Required(ErrorMessage = "Please enter a title for this movie.")]
-        [Display(Name = "Title:")]
-        public string Title { get; set; }
+public class AddMovieVM
+{
+    [Required(ErrorMessage = "Please enter a title for this movie.")]
+    [Display(Name = "Title:")]
+    public string Title { get; set; }
 
-        [Required(ErrorMessage = "Please select a release date for this movie.")]
-        [Display(Name = "Release Date:")]
-        public DateTime ReleaseDate { get; set; }
+    [Required(ErrorMessage = "Please select a release date for this movie.")]
+    [Display(Name = "Release Date:")]
+    public DateTime ReleaseDate { get; set; }
 
-        [Required(ErrorMessage = "Please enter a running time for this movie")]
-        [Range(1, 1000, ErrorMessage = "The running time must be between 1 and 1000 minutes.")]
-        [Display(Name = "Running Time:")]
-        public int RunningTime { get; set; }
+    [Required(ErrorMessage = "Please enter a running time for this movie")]
+    [Range(1, 1000, ErrorMessage = "The running time must be between 1 and 1000 minutes.")]
+    [Display(Name = "Running Time:")]
+    public int RunningTime { get; set; }
 
-        [Display(Name = "Description:")]
-        public string Description { get; set; }
+    [Display(Name = "Description:")]
+    public string Description { get; set; }
 
-        [Display(Name = "Genre:")]
-        public int SelectedGenre { get; set; }
-        public List<SelectListItem> AllGenres { get; set; }
-    }
+    [Display(Name = "Genre:")]
+    public int SelectedGenre { get; set; }
+
+    public List<SelectListItem> AllGenres { get; set; }
+}
 }
